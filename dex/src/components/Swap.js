b0e6900dc84c5fc4fd52f842aed251d21d6e6d87
mazmann <sadi.lundberg@hotmail.com>
2024-03-10 22:04:31 +0100

    added tokenselection

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -13,6 +13,10 @@ function Swap() {
   const [slippage, setSlippage] = useState(2.5);
   const [tokenOneAmount, setTokenOneAmount] = useState(null);
   const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
+  const [tokenOne, setTokenOne] = useState(tokenList[0]);
+  const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [changeToken, setChangeToken] = useState(1);
 
 
   function handleSlippage(e) {
@@ -22,6 +26,19 @@ function Swap() {
   function changeAmount(e) {
     setTokenOneAmount(e.target.value);
   }
+
+  function switchTokens() {
+    const one = tokenOne;
+    const two = tokenTwo;
+    setTokenOne(two);
+    setTokenTwo(one);
+  }
+
+  function openModal(asset) {
+    setChangeToken(asset);
+    setIsOpen(true);
+  }
+
   const settings = (
     <>
       <div>Slippage Tolerance</div>
@@ -36,26 +53,64 @@ function Swap() {
   )
 
   return (
-    <div className='tradeBox'>
-      <div className='tradeBoxHeader'>
-        <h4>Swap</h4>
-        <Popover
-          content={settings}
-          title="Settings"
-          trigger="click"
-          placement='bottomRight'>
-          <SettingOutlined className='cog' />
-        </Popover>
+    <>
+      <Modal
+        open={isOpen}
+        footer={null}
+        onCancel={() => setIsOpen(false)}
+        title="Select a token"
+      >
+        <div className='modalContent'>
+          {tokenList?.map((e, i) => {
+            return (
+              <div className='tokenChoice'
+                key={i}
+                // onClick={() => modifyToken(i)}
+                >
+                  <img src={e.img} alt={e.ticker} className='tokenLogo' />
+                  <div className='tokenChoiceNames'>
+                    <div className='tokenName'>{e.name}</div>
+                    <div className='tokenTicker'>{e.ticker}</div>
+                  </div>
+
+              </div>
+            )
+          })}
+        </div>
+      </Modal>
+      <div className='tradeBox'>
+        <div className='tradeBoxHeader'>
+          <h4>Swap</h4>
+          <Popover
+            content={settings}
+            title="Settings"
+            trigger="click"
+            placement='bottomRight'>
+            <SettingOutlined className='cog' />
+          </Popover>
+        </div>
+        <div className='inputs'>
+          <Input placeholder="0" value={tokenOneAmount}
+            onChange={changeAmount} />
+          <Input placeholder="0" value={tokenTwoAmount}
+            disabled={true} />
+          <div className='switchButton' onClick={switchTokens}>
+            <ArrowDownOutlined className='switchArrow' />
+          </div>
+          <div className='assetOne' onClick={() => openModal(1)}>
+            <img src={tokenOne.img} alt="tokenOne" className="assetLogo" />
+            {tokenOne.ticker}
+            <DownOutlined />
+          </div>
+
+          <div className='assetTwo' onClick={() => openModal(2)}>
+            <img src={tokenTwo.img} alt="tokenTwo" className="assetLogo" />
+            {tokenTwo.ticker}
+            <DownOutlined />
+          </div>
+        </div>
       </div>
-      <div className='inputs'>
-        <Input placeholder="0" value={tokenOneAmount}
-          onChange={changeAmount} />
-        <Input placeholder="0" value={tokenTwoAmount}
-          disabled={true} />
-        <div className='assetOne'></div>
-        <div className='assetTwo'></div>
-      </div>
-    </div>
+    </>
   )
 }
 
