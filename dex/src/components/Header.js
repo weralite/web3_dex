7193c6e90b1c14359d98bfe15268cb636c11be13
mazmann <sadi.lundberg@hotmail.com>
2024-03-11 16:37:13 +0100

    added metamask connection

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -3,7 +3,10 @@ import Logo from "../moralis-logo.svg";
 import Eth from "../eth.svg";
 import { Link } from "react-router-dom";
 
-function Header() {
+function Header(props) {
+
+  const {address, isConnected, connect} = props;
+
   return (
     <header>
       <div className='leftH'>
@@ -22,8 +25,8 @@ function Header() {
           <img src={Eth} alt="Ethereum Logo" className='eth' />
           Ethereum
         </div>
-        <div className='connectButton'>
-          Connect
+        <div className='connectButton' onClick={connect}>
+          {isConnected ? (address.slice(0,4) +"..." +address.slice(38)) : "Connect"}
         </div>
 
       </div>
