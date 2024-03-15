7c9197f58c62b52148061e511db1651d96b4048a
mazmann <sadi.lundberg@hotmail.com>
2024-03-15 09:45:37 +0100

    price fetch works

--- dexBack/index.js
+++ dexBack/index.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const app = express();
 const port = 3001;
 
-const apiUrl = 'https://api.1inch.dev/swap/v6.0/1/tokens';
+const tokenList = 'https://api.1inch.dev/swap/v6.0/1/tokens';
+const tokenPrice = 'https://api.1inch.dev/price/v1.1/1/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 const apiKey = 'wdkgxDpkCD2ZfzOmzuoiC3Xas2rYHljc';
 
 const headers = {
@@ -19,8 +20,21 @@ app.use(
   }));
 
 
+app.get('/tokenList', (req, res) => {
+  axios.get(tokenList, { headers })
+    .then(response => {
+      // Handle the API response here
+      res.json(response.data);
+    })
+    .catch(error => {
+      // Handle errors
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+});
+
 app.get('/tokenPrice', (req, res) => {
-  axios.get(apiUrl, { headers })
+  axios.get(tokenPrice, { headers })
     .then(response => {
       // Handle the API response here
       res.json(response.data);
