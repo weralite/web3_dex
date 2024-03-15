d5825af6d5d55c0eb56366a7f06e3bda826807cc
mazmann <sadi.lundberg@hotmail.com>
2024-03-15 09:13:27 +0100

    switched to 1inch api key

--- dexBack/index.js
+++ dexBack/index.js
@@ -1,38 +1,39 @@
-const express = require("express");
-const Moralis = require("moralis").default;
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
 const app = express();
-const cors = require("cors");
-require("dotenv").config();
 const port = 3001;
 
-app.use(cors());
-app.use(express.json());
+const apiUrl = 'https://api.1inch.dev/swap/v6.0/1/tokens';
+const apiKey = 'wdkgxDpkCD2ZfzOmzuoiC3Xas2rYHljc';
 
-app.get("/tokenPrice", async (req, res) => {
+const headers = {
+  'accept': 'application/json',
+  'Authorization': `Bearer ${apiKey}`
+};
 
-  const { query } = req;
-
-  const responseOne = await Moralis.EvmApi.token.getTokenPrice({
-    address: query.addressOne
-  })
-
-  const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
-    address: query.addressTwo
-  })
-
-  const usdPrices = {
-    tokenOne: responseOne.raw.usdPrice,
-    tokenTwo: responseTwo.raw.usdPrice,
-    ratio: responseOne.raw.usdPrice / responseTwo.raw.usdPrice
-  }
+app.use(
+  cors({
+      origin: "http://localhost:3000",
+  }));
 
 
-  return res.status(200).json(usdPrices);
+app.get('/tokenPrice', (req, res) => {
+  axios.get(apiUrl, { headers })
+    .then(response => {
+      // Handle the API response here
+      res.json(response.data);
+    })
+    .catch(error => {
+      // Handle errors
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
 });
-Moralis.start({
-  apiKey: process.env.MORALIS_KEY,
-}).then(() => {
-  app.listen(port, () => {
-    console.log(`Listening for API Calls`);
-  });
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
